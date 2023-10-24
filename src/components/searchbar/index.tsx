0f1627b70209b74ec23234888fe01aef6a1a3b49
Buzuev1234 <buzuevaleksandr71@gmail.com>
2023-10-24 11:21:08 +0900

    "updated tooltip"

--- src/components/searchbar/index.tsx
+++ src/components/searchbar/index.tsx
@@ -31,11 +31,17 @@ const SearchBar = ()=>{
                    </div>
                 </div>
             </div>
-            <div className="basis-1/12">
-                <div className="w-[63px] h-[63px] flex justify-center items-center rounded-full bg-mic">
-                    <img
-                        src="/img/icons/mic.svg"
-                    />
+            <div className="basis-3/12" >
+                {/* <div className="relative w-[63px] h-[63px] flex justify-center items-center rounded-full bg-mic hover:cursor-pointer">
+                    <img src="/img/icons/mic.svg" />
+                    <div className="absolute bottom-[80px] left-1/2 -translate-x-1/2 bg-white text-black px-2 py-2 rounded-full shadow-lg whitespace-nowrap tooltip-rotate">
+                        <span style={{fontSize:"14px", fontFamily:"sans-serif"}}>Say "<strong >Hi Stefan!</strong>"</span>
+                        <span className="tooltip-triangle"></span>
+                    </div>
+                </div> */}
+                <div className="relative flex justify-center items-center rounded-full hover:cursor-pointer aspect-w-1 aspect-h-1">
+                    <img src="/img/icons/mic.svg" className="w-[63px] h-[63px]"/>
+                    <img src="/img/union.png" alt="Union Icon" className="absolute bottom-[40px] tooltip-settig"/>
                 </div>
             </div>
         </div>
